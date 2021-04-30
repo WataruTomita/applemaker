0dbd1540db61a91b85e9bd5e0e9f682edb4ebe24
WataruTomita <tomi.symphonia@gmail.com>
2021-04-30 23:02:18 +0900

    top page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'home/new'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'homes#top'
+  get 'about', to: 'homes#about'
 end
